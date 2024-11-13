aa765a8519d679f1e037822c989c533711e10e63
Giovanni Silva <git@gsilva.pro>
2024-11-13 19:59:15 -0300

    formulario reativo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,31 +1,53 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, 
-  IonTitle, IonContent, IonInput, IonItem, IonList,
-  IonSelect,
-  IonSelectOption, IonButton } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput, IonItem, IonList,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
+  templateUrl: './tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
   imports: [IonButton, IonList, IonSelect, IonSelectOption, IonItem, IonInput, IonHeader, IonToolbar, 
     IonTitle, IonContent, ExploreContainerComponent,
-    FormsModule,
+    ReactiveFormsModule,
     CommonModule
   ],
 })
 export class Tab1Page {
 
-  pessoa: any = {}
+  msg = ""
+  formGroup: FormGroup
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      nome: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', Validators.email],
+      hoobie: ['']
+    })
+  }
 
   salvar() {
-    console.log("Salvando....")
-    console.log(this.pessoa)
+    if(this.formGroup.valid) {
+      console.log("Salvando....")
+      console.log(this.formGroup.value)
+      this.formGroup.reset()
+      this.msg = ""
+    } else {
+      console.error('Invalid')
+      this.msg = "Erro de validação"
+    }
+    
   }
 }
