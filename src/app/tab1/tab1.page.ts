d479da15faac0ba3637dac33c9d416ef78d75b83
Giovanni Silva <git@gsilva.pro>
2024-11-12 21:50:14 -0300

    pegando info do formulario

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,14 +4,28 @@ import { IonHeader, IonToolbar,
   IonSelect,
   IonSelectOption, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonButton, IonList, IonSelect, IonSelectOption, IonItem, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButton, IonList, IonSelect, IonSelectOption, IonItem, IonInput, IonHeader, IonToolbar, 
+    IonTitle, IonContent, ExploreContainerComponent,
+    FormsModule,
+    CommonModule
+  ],
 })
 export class Tab1Page {
+
+  pessoa: any = {}
+
   constructor() {}
+
+  salvar() {
+    console.log("Salvando....")
+    console.log(this.pessoa)
+  }
 }
