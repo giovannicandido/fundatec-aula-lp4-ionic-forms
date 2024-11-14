ef02870a9900838870d7ffdfc197f8bbec58574f
Giovanni Silva <git@gsilva.pro>
2024-11-13 21:13:24 -0300

    lista de pessoas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonContent, IonHeader, IonItem, IonList, IonTitle, IonToolbar, ViewDidEnter } from '@ionic/angular/standalone';
+import { Pessoa } from '../model/pessoa';
+import { PessoaService } from '../services/pessoa.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent,
+    CommonModule
+  ]
 })
-export class Tab2Page {
-
-  constructor() {}
+export class Tab2Page implements OnInit, ViewDidEnter {
+  pessoas: Pessoa[] = []
+  constructor(private service: PessoaService) {}
+  
+  ionViewDidEnter(): void {
+    this.pessoas = this.service.listAll()
+    console.log("Ionic View Did Enter")
+  }
+  
+  ngOnInit(): void {
+    console.log("NG On Init")
+  }
 
 }
